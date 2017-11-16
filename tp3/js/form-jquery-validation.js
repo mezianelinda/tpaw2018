d71db853494b429c0b300c4386f091ad3e0dfbcd
mezianelinda <32832948+mezianelinda@users.noreply.github.com>
2017-11-16 08:25:29 +0100

    Add files via upload

---
+++ tp3/js/form-jquery-validation.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+   // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+   // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+    console.log( "DOM ready!" );
+    $("#submit").on("click",function(event){ 
+  event.preventDefault(); 
+  console.log( "click" ); 
+    $('#myModal').modal("show");
+});
+
+    
+    
+});
